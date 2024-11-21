e9513007c42711ccee61f28a4644d8aae8242f26
Max Winokan <mwinokan@me.com>
2024-11-21 14:40:03 +0000

    add header block

--- merge_cif.py
+++ merge_cif.py
@@ -2,6 +2,16 @@ from pathlib import Path
 import mrich
 from gemmi import cif
 
+HEADER_LOOP_COLUMNS = [
+    "_chem_comp.id",
+    "_chem_comp.three_letter_code",
+    "_chem_comp.name",
+    "_chem_comp.group",
+    "_chem_comp.number_atoms_all",
+    "_chem_comp.number_atoms_nh",
+    "_chem_comp.desc_level",
+]
+
 
 def merge_cifs(infiles: Path, outfile: Path):  # , code_map: dict[str,str]):
 
@@ -25,6 +35,16 @@ def merge_cifs(infiles: Path, outfile: Path):  # , code_map: dict[str,str]):
 
         for item in block:  # This iterates through all data in the block
             new_block.add_item(item)
+
+    # create the header block
+    outdoc.add_new_block("comp_list", 0)
+    header_block = outdoc.find_block("comp_list")
+    header_loop = header_block.init_loop("_chem_comp", HEADER_LOOP_COLUMNS)
+
+    # populate the header block
+    for indoc in indocs:
+        lig_name = indoc.sole_block().name.removeprefix("data_")
+        header_loop.add_row([lig_name, lig_name, "?", "?", "?", "?", "?"])
 
     # Save the target document if needed
     outdoc.write_file(str(outfile.resolve()))
